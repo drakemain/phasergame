0fc68fc09e873042102b39d9e7fadc907c8d172f
drakemain <demain1990@gmail.com>
2016-08-08 16:29:45 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Using:
+
+phaser.io
+github.com/lean/phaser-es6-webpack
---
+++ src/states/Splash.js
@@ -0,0 +1,29 @@
+import Phaser from 'phaser'
+import { centerGameObjects } from '../utils'
+
+export default class extends Phaser.State {
+  init () {}
+
+  preload () {
+    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
+    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
+    centerGameObjects([this.loaderBg, this.loaderBar])
+
+    this.load.setPreloadSprite(this.loaderBar)
+    //
+    // load your assets
+    //
+    this.load.image('mushroom', 'assets/images/mushroom2.png');
+    this.load.audio('gameMusic', ['assets/audio/splash.mp3']);
+  }
+
+  create () {
+    this.loaderBar.cropEnabled = false;
+  }
+
+  update () {
+    if (this.cache.isSoundDecoded('gameMusic')) {
+      this.state.start('Game');
+    }
+  }
+}
