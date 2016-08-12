0b3c2bf159cf9a04cbff217797e1c7000bc0041a
drakemain <demain1990@gmail.com>
2016-08-11 21:49:31 -0700

    Add player and small room
    Arrow keys can move a sprite.
    
    There is collision between the player sprite and the walls.

--- src/states/Game.js
+++ src/states/Game.js
@@ -8,7 +8,20 @@ export default class extends Phaser.State {
   preload () {}
 
   create () {
-    let banner = this.add.text(this.game.world.centerX, this.game.height - 30, 'Phaser + ES6 + Webpack')
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+
+    this.inputKeys = this.input.keyboard.createCursorKeys();
+
+    this.walls = this.add.physicsGroup();
+    this.walls.enableBody = true;
+
+    this.player = this.game.add.sprite(
+      this.game.world.centerX
+      , this.game.world.centerY
+      , 'diamond');
+    this.physics.arcade.enable(this.player);
+
+    let banner = this.add.text(this.game.world.centerX, this.game.height - 30, 'Awesome Game 10/10 5 stars');
     banner.font = 'Nunito'
     banner.fontSize = 40
     banner.fill = '#77BFA3'
@@ -17,21 +30,68 @@ export default class extends Phaser.State {
     this.music = this.add.audio('gameMusic');
     this.music.play();
 
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.game.world.centerX,
-      y: this.game.world.centerY,
-      asset: 'mushroom'
-    })
+    this.topWall = this.createWall(
+      this.game.world.centerX - 200
+      , this.game.world.centerY - 216
+      , 'wall'
+    );
+    this.bottomWall = this.createWall(
+      this.game.world.centerX - 200
+      , this.game.world.centerY + 216
+      , 'wall'
+    );
+    this.rightWall = this.createWall(
+      this.game.world.centerX + 168
+      , this.game.world.centerY - 184
+      , 'vertWall'
+    );
+    this.leftWall = this.createWall(
+      this.game.world.centerX - 200
+      , this.game.world.centerY - 184
+      , 'vertWall'
+    );
 
     // set the sprite width to 30% of the game width
-    setResponsiveWidth(this.mushroom, 30, this.game.world)
-    this.game.add.existing(this.mushroom)
+    // setResponsiveWidth(this.mushroom, 30, this.game.world)
+    // this.game.add.existing(this.mushroom)
+  }
+
+  update () {
+    this.playerMovement();
+
+    this.physics.arcade.collide(this.player, this.walls)
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      this.game.debug.spriteInfo(this.player, 32, 32)
     }
+  }
+
+  playerMovement() {
+    this.player.body.velocity.x = 0;
+    this.player.body.velocity.y = 0;
+
+    if (this.inputKeys.left.isDown) {
+      this.player.body.velocity.x = -100;
+    } else if (this.inputKeys.right.isDown) {
+      this.player.body.velocity.x = 100;
+    }
+
+    if (this.inputKeys.up.isDown) {
+      this.player.body.velocity.y = -100;
+    } else if (this.inputKeys.down.isDown) {
+      this.player.body.velocity.y = 100;
+    }
+  }
+
+  createWall(x, y, asset) {
+    let wall = this.add.sprite(x, y, asset);
+
+    this.walls.add(wall);
+
+    wall.body.immovable = true;
+
+    return wall;
   }
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -2,7 +2,10 @@ import Phaser from 'phaser'
 import { centerGameObjects } from '../utils'
 
 export default class extends Phaser.State {
-  init () {}
+  init () {
+    this.loaderBg;
+    this.loaderBar;
+  }
 
   preload () {
     this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
@@ -13,6 +16,10 @@ export default class extends Phaser.State {
     //
     // load your assets
     //
+    
+    this.load.image('diamond', 'assets/test/diamond.png');
+    this.load.image('wall', 'assets/test/platform.png');
+    this.load.image('vertWall', 'assets/test/platformVert.png');
     this.load.image('mushroom', 'assets/images/mushroom2.png');
     this.load.audio('gameMusic', ['assets/audio/splash.mp3']);
   }
